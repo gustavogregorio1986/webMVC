15c50a9de26d90b5a0aab464fbb779a47035e6fd
guga56 <gustavogregorio086@gmail.com>
2022-07-22 20:55:20 -0300

    declaração dos servços no controller

--- WebMVC/Controllers/CadastroController.cs
+++ WebMVC/Controllers/CadastroController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebMVC.Dominio.Dominio;
+using WebMVC.Models;
+using WebMVC.Servico.Servico;
 
 namespace WebMVC.Controllers
 {
     public class CadastroController : Controller
     {
+        private readonly AeroportoServico _servicoAeroprto;
+        private readonly AviaoServico _servicoAviao;
+        private readonly DestinoServico _servicoDestino;
+        private readonly EnderecoServico _servicoEndereco;
+        private readonly OrigemServico _servicoOrigem;
+        private readonly PassageiroServico _servicoPassagero;
+
         public IActionResult Cadastrar()
         {
             return View();
         }
+
+        
     }
 }
--- WebMVC/Controllers/PesquisaController.cs
+++ WebMVC/Controllers/PesquisaController.cs
@@ -4,6 +4,14 @@ namespace WebMVC.Controllers
 {
     public class PesquisaController : Controller
     {
+
+        private readonly AeroportoServico _servicoAeroprto;
+        private readonly AviaoServico _servicoAviao;
+        private readonly DestinoServico _servicoDestino;
+        private readonly EnderecoServico _servicoEndereco;
+        private readonly OrigemServico _servicoOrigem;
+        private readonly PassageiroServico _servicoPassagero;
+
         public IActionResult Pesquisar()
         {
             return View();
